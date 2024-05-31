53aac02f614d8880f12341fdc47f9efce1a565e9
rayoulb <166954850+rayoulb@users.noreply.github.com>
2024-05-31 21:29:46 +0200

    Update conn.php

--- database/conn.php
+++ database/conn.php
@@ -1,19 +1,12 @@
 <?php 
 
 session_start();
-define("ROOT","https://rcoding.xyz/");
+define("ROOT","localhost/csh-main/");
 define("NAME_WEBSITE","CSH");
-$conn= mysqli_connect('localhost','u306861737_csh','Bg+7A7N$h','u306861737_csh');
+$conn= mysqli_connect('localhost','root','','csh');
 
 
-if (!isset($_SESSION['cart'])) {
-    $_SESSION['cart'] = array();
-  }
-  
-  //unset qunatity
-  unset($_SESSION['qty_array']);
 
-  
   
   
 
